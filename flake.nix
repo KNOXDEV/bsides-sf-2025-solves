ee83938bc7dd364ea222663101fe62b86660443f
Nick Knox <nick@knox.codes>
2026-02-08 19:12:06 -0800

    solve: pascals-homomorphism-1

--- flake.nix
+++ flake.nix
@@ -11,33 +11,49 @@
   }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+
     python = pkgs.python312;
     pythonPackages = with python.pkgs; [
-      pycryptodome
+      # image manipulation
       wand
+
+      # math stuff for crypto challenges.
+      # Note that certain things simply require SageMath,
+      # which is only packaged as its own venv, and has to be accessed via
+      # the wrapped `sage` cli for now.
+      pycryptodome
       numpy
+      sympy
+
+      # socket shenanigans
+      pwntools
     ];
   in {
     devShells.x86_64-linux.default = pkgs.mkShell {
-      packages = [
-        pkgs.deno
-        pkgs.basedpyright
+      packages =
+        [
+          pkgs.deno
+          pkgs.basedpyright
 
-        # cli tools for steg/forensics
-        pkgs.exiftool
-        pkgs.binwalk
-        pkgs.imagemagick
-        
-        # netcat, telnet, etc
-        pkgs.inetutils
-        pkgs.socat
+          # cli tools for steg/forensics
+          pkgs.exiftool
+          pkgs.binwalk
+          pkgs.imagemagick
 
-        # reversing
-        pkgs.jadx   # android
-        pkgs.ghidra # x86_64 executables
+          # netcat, telnet, etc
+          pkgs.inetutils
+          pkgs.socat
 
-        python
-      ] ++ pythonPackages;
+          # reversing
+          pkgs.jadx # android
+          pkgs.ghidra # x86_64 executables
+
+          # cli tools for crypto / math
+          pkgs.sage
+
+          python
+        ]
+        ++ pythonPackages;
     };
   };
 }
